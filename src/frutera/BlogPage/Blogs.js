719d7a82c59c361355ed7118c5138c63aef549a1
LukaViper <86350559+LukaViper@users.noreply.github.com>
2022-08-28 15:17:08 +0400

    Add files via upload

---
+++ src/frutera/BlogPage/Blogs.js
@@ -0,0 +1,14 @@
+import BlogPage from "../components/BlogPage/BlogPage";
+
+import './Blogs.scss';
+
+function Blogs() {
+  return (
+    <>
+    <BlogPage/>
+   
+    </>
+  );
+}
+
+export default Blogs;
