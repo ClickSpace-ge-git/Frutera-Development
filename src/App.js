ca943424845a90584770289a852c70fe311db5cb
LukaViper <86350559+LukaViper@users.noreply.github.com>
2022-08-28 15:23:29 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import HomePage from './frutera/HomePage/HomePage'
 import Blogs from './frutera/BlogPage/Blogs';
 import Product from './frutera/ProductsPage/product';
 import CartPage from "./frutera/CartPage/CartPage";
+import RegisterPage from './frutera/RegisterPage/RegisterPage'
+import LoginPage from './frutera/LoginPage/LoginPage'
+import UserPage from './frutera/UserPage/UserPage'
+import BlogsPage from './frutera/BlogsPage/BlogsPage.js';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.scss';
@@ -13,9 +18,16 @@ export default function App() {
          <Routes>
             <Route path='/' element={<HomePage/>}/>
             <Route path='/aboutus' element={<AboutUsPage/>}/>
-            <Route path='/blogs' element={<Blogs/>}/>
+            <Route path='/blog' element={<Blogs/>}/>
             <Route path='/products' element={<Product/>}/>
              <Route path='/cart' element={<CartPage/>}/>
+             <Route path='/register' element={<RegisterPage/>}/>
+             <Route path='/login' element={<LoginPage/>}/>
+             <Route path='/user' element={<UserPage/>}/>
+             <Route path='/blogs' element={<BlogsPage/>}/>
+
+
+
          </Routes>
       </BrowserRouter>
    );
