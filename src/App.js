19ebf23096a1a4638e84ca7e3416688af8686e54
ALXSAI <alexandersainishvili@gmail.com>
2022-08-28 15:49:13 +0400

    Deleted old Login to allow merging

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Blogs from './frutera/BlogPage/Blogs';
 import ProductsPage from "./frutera/ProductsPage/ProductsPage";
 import CartPage from "./frutera/CartPage/CartPage";
 import Product from "./frutera/Product/Product";
-import LoginPage from "./frutera/LoginPage/LoginPage";
-import RegisterPage from "./frutera/RegisterPage/RegisterPage";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import './App.scss';
@@ -20,8 +18,6 @@ export default function App() {
                 <Route path='/products' element={<ProductsPage/>}/>
                 <Route path='/product' element={<Product/>}/>
                 <Route path='/cart' element={<CartPage/>}/>
-                <Route path='/login' element={<LoginPage/>}/>
-                <Route path='/register' element={<RegisterPage/>}/>
             </Routes>
         </BrowserRouter>
     );
